329e0174a5d250c2432bee69d43dee7465c5b509
Jacob Runyan <jmrunyan@ucsc.edu>
2018-05-15 23:41:16 -0700

    Created a basic database schema.

--- models/tables.py
+++ models/tables.py
@@ -6,8 +6,64 @@
 #       'date','time','datetime','blob','upload', 'reference TABLENAME'
 # There is an implicit 'id integer autoincrement' field
 # Consult manual for more options, validators, etc.
+# http://www.web2pyref.com/reference/field-type-database-field-types
+# http://www.web2py.com/books/default/chapter/29/06/the-database-abstraction-layer @ field constructor
 
+import datetime
 
+def get_user_email():
+    return auth.user.email if auth.user is not None else None
+
+db.define_table('users',
+                Field('user_email', default=get_user_email()),
+                Field('username', 'text', required=True),
+                Field('password', 'text', required=True),
+                Field('account_creation_date', 'date', update=datetime.date.today()),
+                )
+
+db.define_table('game2_prompts',
+                Field('prompt', 'text', required=True),
+                )
+
+db.define_table('game3_words',
+                Field('word', 'string', required=True),
+                )
+
+db.define_table('game1_instances', #Tall Tales
+                #default game attributes
+                Field('room_code', 'text', default = ''),
+                Field('player_list', 'list:integer', 'references users'), #ref user id's
+                Field('hoster', 'references users', required=True),
+                Field('max_players', 'integer', default=10),
+                Field('turn_time_limit', 'integer', default=30),
+                #game specific attributes
+                Field('story_text', 'list:string', default=[]),
+                )
+
+db.define_table('game2_instances', #Type Racer
+                #default game attributes
+                Field('room_code', 'text', default = ''),
+                Field('player_list', 'list:integer', 'references users'), #ref user id's
+                Field('hoster', 'references users', required=True),
+                Field('max_players', 'integer', default=10),
+                Field('turn_time_limit', 'integer', default=30),
+                #game specific attributes
+                Field('prompt', 'references game2_prompts'),
+                Field('player_seedings', 'list:integer', default=[]),
+                )
+
+db.define_table('game3_instances', #Taboo
+                #default game attributes
+                Field('room_code', 'string', default = ''),
+                Field('player_list', 'list:integer', 'references users'), #ref user id's
+                Field('hoster', 'references users', required=True),
+                Field('max_players', 'integer', default=10),
+                Field('turn_time_limit', 'integer', default=30),
+                #game specific attributes
+                Field('banned_words', 'list:string', default=[]),
+                Field('secret_word', 'string', 'references game3_words'),
+                Field('player_scores', 'list:integer', default=[]),
+                )
 
 
 # after defining tables, uncomment below to enable auditing

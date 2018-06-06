c51863287d980045808477d431766974bb1c610e
Jacob Runyan <jmrunyan@ucsc.edu>
2018-06-05 19:06:35 -0700

    Moved each game's html files into their own folders and created new controllers so that URLs are nicer.

---
+++ controllers/taboo.py
@@ -0,0 +1,9 @@
+#### A Controller for the Taboo game mode. ####
+
+def game():
+    print("In a Taboo game!!!!")
+    return dict(hi="hi")
+
+def join():
+    print("At Taboo join screen.")
+    return dict(hi="hi")
